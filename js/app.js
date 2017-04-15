e34021121f19513afe8fe6584c96d7833c1770ed
Geoffrey Caven <geoffrey@bravenew.world>
2017-04-15 17:35:31 -0300

    commit work so far

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var m_names = new Array("Jan", "Feb", "Mar",
+"Apr", "May", "Jun", "Jul", "Aug", "Sep",
+"Oct", "Nov", "Dec");
+
+var d_names = new Array("Sun", "Mon", "Tues",
+"Wed", "Thurs", "Fri", "Sat");
+
+function leftPad(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function convertTo12(i) {
+  if (i > 12) {
+    i = i-12;
+  }
+  return i;
+}
+
+function getAmPm(i) {
+  if (i > 12) {
+    return "PM";
+  } else {
+    return "AM";
+  }
+}
+
+
+var d = new Date();
+
+var curr_hour = d.getHours();
+var curr_minute = d.getMinutes();
+var time_string = convertTo12(curr_hour) + ":" + leftPad(curr_minute) + " " + getAmPm(curr_hour);
+
+document.getElementById("current-time").innerHTML = time_string;
+
+var curr_day = d.getDay();
+var curr_date = d.getDate();
+var curr_month = d.getMonth();
+var curr_year = d.getFullYear();
+var date_string = d_names[curr_day] + " " + curr_date + " " + m_names[curr_month] + ", " + curr_year;
+
+document.getElementById("current-date").innerHTML = date_string;
+
+// GOOGLE SEARCH FUNCTIONALITY
+
+document.getElementById('google-input').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+      var query = this.value;
+      query = query.replace(/\s+/g, "+");
+      query = "https://www.google.ca/#q=" + query;
+      window.location.href = query;
+    }
+}
